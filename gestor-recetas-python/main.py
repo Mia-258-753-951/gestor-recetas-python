fcd32cfea8b00ec24a40c3fb0113d038b5236b39
fernando del pino <fdplopez@gmail.com>
2025-11-18 22:30:05 +0100

    Inicio del proyecto Gestor de Recetas

---
+++ gestor-recetas-python/main.py
@@ -0,0 +1,7 @@
+#gestor-recetas-python/main.py
+
+def main():
+    print('Gestor de recetas - inicio del proyecto')
+    
+if __name__ == '__main__':
+    main()
---
+++ gestor-recetas-python/readme.md
@@ -0,0 +1,3 @@
+# Gestor de Recetas
+
+Proyecto en Python para gestionar recetas y generar la lista de la compra.
